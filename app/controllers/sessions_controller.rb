a063c14ea337918f4c9911e4aede781a06d46c97
anju-kumaran <anju.naduth@gmail.com>
2021-11-11 19:48:19 -0500

    Added new feature user:authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    # If login successful
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If login failed Redirect to login page
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+end
